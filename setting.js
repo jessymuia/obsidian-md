14dac913a9b6a3037fc55975c58f8d4ae6f8e965
Jessy Muia <kiambachris29@gmail.com>
2026-06-15 18:52:01 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,10 +1,10 @@
 //===== Obsidian md====
 
 module.exports = {
-  BOT_NAME: 'ᴏʙsɪᴅɪᴀɴ ᴍᴅ', // ur bot name
-  OWNER_NAME: 'ᴛʜᴇ sᴛɪᴀɴ', //your name
-  OWNER_NUMBER: '', // add your phone number
-  SESSION_ID: '', // use sessin id if inbuilt pairing not working
-  MODE: "public", // public or private
+  BOT_NAME: 'Jespher', // ur bot name
+  OWNER_NAME: 'Jespher', //your name
+  OWNER_NUMBER: '0710318560', // add your phone number
+  SESSION_ID: 'Stian~AH48cR4vzHSS', // use sessin id if inbuilt pairing not working
+  MODE: "private", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
 };
